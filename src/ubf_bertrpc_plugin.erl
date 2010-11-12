34eb2c088495abfb35b05537c9453388061aa2e7
Joseph Wayne Norton <norton@alum.mit.edu>
2010-11-12 12:45:57 +0900

    Add new -type and -spec declarations for subset of server and plugin related functions.

--- src/ubf_bertrpc_plugin.erl
+++ src/ubf_bertrpc_plugin.erl
@@ -1,12 +1,13 @@
 %%% -*- mode: erlang -*-
-%%% @doc Sample BERT-RPC contract.
+%%% @doc Sample BERT-RPC plugin.
 %%%
 %%%
 
 -module(ubf_bertrpc_plugin).
 -behavior(ubf_plugin_stateless).
 
-%% Required callback API for all UBF contract implementations.
+%% Required (except keepalive/0) callback API for UBF stateless
+%% implementations.
 -export([info/0, description/0, keepalive/0]).
 -export([handlerStart/1, handlerStop/3, handlerRpc/1, handlerEvent/1]).
 
@@ -16,6 +17,7 @@
 -add_contract("ubf_bertrpc_plugin").
 
 -include("ubf.hrl").
+-include("ubf_plugin_stateless.hrl").
 
 info() ->
     "I am a BERT-RPC server".
@@ -26,37 +28,25 @@ description() ->
 keepalive() ->
     ok.
 
-
-%% @spec handlerStart(Args::list(any())) ->
-%%          {accept, Reply::any(), StateName::atom(), StateData::term()} | {reject, Reason::any()}
 %% @doc start handler
 handlerStart(_Args) ->
     ack = install_handler(self(), fun handlerEvent/1),
     {accept,ok,none,unused}.
 
-%% @spec handlerStop(Pid::pid(), Reason::any(), StateData::term()) -> void()
 %% @doc stop handler
 handlerStop(_Pid, _Reason, _StateData) ->
     unused.
 
-
-%% @spec handlerRpc(Event::any()) ->
-%%          Reply::any()
 %% @doc rpc handler
-%% handlerRpc(Event) ->
 %% @TODO Implement BERT-RPC 1.0 synchronous events
-%% todo;
-handlerRpc(Event)
-  when Event==info; Event==description ->
+handlerRpc(Event) when Event==info; Event==description ->
     ?S(?MODULE:Event());
-handlerRpc(Event)
-  when Event==keepalive ->
-    ?MODULE:Event();
-handlerRpc(Event) ->
-    {Event, not_implemented}.
+handlerRpc(Event) when Event==keepalive ->
+    ?MODULE:Event().
 
+%% @doc event handler
+%% @TODO: Implement BERT-RPC 1.0 asynchronous events
 handlerEvent(Event) ->
-    %% @TODO: Implement BERT-RPC 1.0 asynchronous events
     %% Let's fake it and echo the request
     sendEvent(self(), Event),
     fun handlerEvent/1.
