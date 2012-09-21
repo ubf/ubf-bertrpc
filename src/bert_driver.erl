19c7c8b2a57c7dfbbf3f29da111783fa3d3ec2e4
Joseph Wayne Norton <norton@alum.mit.edu>
2012-09-22 00:50:13 +0900

    Fix and cleanup contract drivers' decoder callback

--- src/bert_driver.erl
+++ src/bert_driver.erl
@@ -8,7 +8,7 @@
 -module(bert_driver).
 -behaviour(contract_driver).
 
--export([start/1, start/2, init/1, init/2, encode/3, decode/5]).
+-export([start/1, start/2, init/1, init/2, encode/3, decode/4]).
 
 start(Contract) ->
     start(Contract, []).
@@ -20,12 +20,10 @@ init(Contract) ->
     init(Contract, []).
 
 init(_Contract, Options) ->
-    {Options, undefined}.
+    {Options, {init, undefined, undefined}}.
 
 encode(_Contract, _Options, Term) ->
     erlang:term_to_binary(Term).
 
-decode(_Contract, Options, undefined, Binary, CallBack) ->
-    Term = erlang:binary_to_term(Binary, Options),
-    CallBack(Term),
-    undefined.
+decode(_Contract, Options, {init, undefined, undefined}, Binary) ->
+    {done, erlang:binary_to_term(Binary, Options), undefined, undefined}.
